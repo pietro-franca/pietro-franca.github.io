e1942fb313c10ed235448ab7f06690ccb3d864dc
Pietro Franca <pietrofranca27@gmail.com>
2025-09-23 10:22:52 -0300

    feat: add Experiences, Projects and Contact sections, and also add hovers and styles

--- src/main.js
+++ src/main.js
@@ -1,2 +1,30 @@
-import '../src/scss/styles.scss'
+emailjs.init("3c6FPHGZ5rZtxcToJ");
 
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const serviceID = "service_lu6su1k";
+    const templateID = "template_ahj2x0q";
+
+    emailjs.sendForm(serviceID, templateID, this).then(
+      () => {
+        document
+          .getElementById("formAlert")
+          .classList.remove("d-none", "alert-danger");
+        document.getElementById("formAlert").classList.add("alert-success");
+        document.getElementById("formAlert").innerText =
+          "Message sent successfully!";
+        this.reset();
+      },
+      (err) => {
+        document
+          .getElementById("formAlert")
+          .classList.remove("d-none", "alert-success");
+        document.getElementById("formAlert").classList.add("alert-danger");
+        document.getElementById("formAlert").innerText =
+          "Failed to send message. Please try again.";
+      }
+    );
+  });
